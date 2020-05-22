4310071c0b1567b21fdcd46a7cf3f9592c901cf0
Medico Alexandre <alexandre.medico@hotmail.fr>
2020-05-22 17:30:47 +0200

    modif css menu

--- front/js/menu.js
+++ front/js/menu.js
@@ -2,11 +2,11 @@ let menu = document.getElementById('menu-burger');
 document.getElementById('new-game').addEventListener('click', function () {
     menu.insertAdjacentHTML("beforeend", `
     <div id="sidebarMenu">
-    <ul class="sidebarMenuInner">
-    <li><a href="./view/jeu.html">Facile</a></li>
-    <li><a href="./view/jeu.html">Moyen</a></li>
-    <li><a href="./view/jeu.html">Difficile</a></li>
-    </ul>
+        <ul class="sidebarMenuInner">
+        <li><a href="./view/jeu.html">Facile</a></li>
+        <li><a href="./view/jeu.html">Moyen</a></li>
+        <li><a href="./view/jeu.html">Difficile</a></li>
+        </ul>
     </div>`);
     menu.classList.add('slide-in-right');
 })
