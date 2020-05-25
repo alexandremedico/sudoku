966103554b268626ce64f2fef5ae84914eb6f93c
Medico Alexandre <alexandre.medico@hotmail.fr>
2020-05-26 00:40:57 +0200

    modif menu

--- front/js/menu.js
+++ front/js/menu.js
@@ -6,7 +6,17 @@ document.getElementById('new-game').addEventListener('click', function () {
         <li><a href="./view/jeu.html">Facile</a></li>
         <li><a href="./view/jeu.html">Moyen</a></li>
         <li><a href="./view/jeu.html">Difficile</a></li>
+        <li><a href="./view/jeu.html">Diaboblique</a></li>
+        <li><a href="./view/jeu.html">Impossible</a></li>
         </ul>
     </div>`);
     menu.classList.add('slide-in-right');
 })
+
+var niveaux = {
+    1 : 17, /*faible*/
+    2 : 15, /*moyen*/
+    3 : 13, /*difficile*/
+    4 : 11, /*diabolique*/
+    5 : 9 /* impossible*/
+};
