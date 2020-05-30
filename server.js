cbb79b1f30621bab99f55bf5b9b6abb447fb6396
Medico Alexandre <alexandre.medico@hotmail.fr>
2020-05-30 16:02:11 +0200

    reception avec socket.io choix difficulté 2

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const http = require('http');
+const socketio = require('socket.io');
+
 const app = express();
 const server = http.Server(app);
+const io = socketio(server);
 const chemin = {
     root: __dirname + '/front'
 };
@@ -12,6 +15,20 @@ app.use(express.static(chemin.root));
 // Routes
 app.get('/', (req, res) => {
     res.render('/index.html');
+})
+
+// socket.io
+let value = '';
+io.on('connection', function (socket) {
+    // console.log('socket open');
+    socket.on('value', function (valueContent) {
+        value = valueContent;
+    })
+
+    socket.on('valueSearch', function () {
+        console.log(value);
+        socket.emit('valueReception', value);
+    })
 })
 
 // Lancement de l'application
